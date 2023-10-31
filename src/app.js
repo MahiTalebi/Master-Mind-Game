cd164eaec188770aabf42b6e73e1ddcdf7070610
Mahi <mhi.talebi2001@gmail.com>
2023-11-01 01:07:58 +0330

    chore: add function for colors

--- src/app.js
+++ src/app.js
@@ -2,12 +2,38 @@ const easyMode = document.querySelector(".easy__button");
 const hardMode = document.querySelector(".hard__button");
 const modal = document.querySelector(".modal");
 
+let targetColors = [];
+let colors = [
+  { name: "red", color: "#e5383b" },
+  { name: "green", color: "#43aa8b" },
+  { name: "blue", color: "#277da1" },
+  { name: "gray", color: "#736f72" },
+  { name: "white", color: "#e5e6e4" },
+  { name: "pink", color: "#e5383b" },
+  { name: "orange", color: "#e5383b" },
+  { name: "violet", color: "#e5383b" },
+];
+
 easyMode.addEventListener("click", showSoftPage);
 hardMode.addEventListener("click", showHardPage);
 
 function showSoftPage() {
+  addTargetColors(5);
   modal.style.transform = "translateX(-100%)";
 }
 function showHardPage() {
+  addTargetColors(7);
   modal.style.transform = "translateX(-100%)";
+}
+
+function addTargetColors(number) {
+  colors = colors.slice(0, number);
+
+  for (let i = 0; i < number; i++) {
+    targetColors.push(getRandom());
+  }
+}
+
+function getRandom() {
+  return colors[Math.floor(Math.random() * colors.length)];
 }
